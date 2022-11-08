204e03d70279d58c9cf362495415ed3120e99261
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-08 16:43:52 +0100

    adds prompt

---
+++ paridispari/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+//L’utente sceglie pari o dispari
+const sceltaUser = prompt("Scegli tra PARI e DISPARI ");
+console.log(sceltaUser);
+//L’utente sceglie numero da 1 a 5
+const numeroUser = Number(prompt("Scegli un numero da 1 a 5"));
+console.log(numeroUser);
