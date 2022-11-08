b513513dca98219ca18844285e5be5e618c5848e
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-11-08 16:53:12 +0100

    adds pari

--- paridispari/js/main.js
+++ paridispari/js/main.js
@@ -5,9 +5,16 @@ const sceltaUser = prompt("Scegli tra PARI e DISPARI ");
 console.log(sceltaUser);
 //L’utente sceglie numero da 1 a 5
 const numeroUser = Number(prompt("Scegli un numero da 1 a 5"));
-console.log("user number " + numeroUser);
+console.log("user number : " + numeroUser);
 
 
 //creo numero random del pc
 const numberPc = Math.floor(Math.random() * 5 + 1);
-console.log("pc number " + numberPc);
+console.log("pc number : " + numberPc);
+
+//capire se somma dei due numeri è pari o dispari
+const sommaNumeri = numeroUser + numberPc;
+console.log("somma numero : " + sommaNumeri);
+// se somma è pari
+let sommaPari = (sommaNumeri % 2 === 0);
+console.log(sommaPari);
